5ce794ff14ac269b24a8c271fa3f72faf56a7c18
Raihan Khalil Abdillah <tatuhey@gmail.com>
2023-02-08 11:14:27 +0800

    Add files via upload

---
+++ LoopDataStructure.py
@@ -0,0 +1,8 @@
+# Raihan Khalil Abdillah, 30065695
+# AT2.4 Question 1
+# Loop Data Structures
+
+carlist = ["Toyota Camry", "Ford Falcon", "Nissan Pulsar", "Ferrari F50", "BMW M5"]
+
+for car in carlist:
+    print(car)
